ff83c70ffc8b0a9434ea442d2418ea9a5be0d225
Alexander Veselov <alexander.veselov@spryker.com>
2026-09-14 12:38:19 +0000

    FRW-11816 Support Symfony 7. (#971)
    FRW-11816 Support Symfony 7.

--- src/Spryker/Zed/Maintenance/Communication/Console/MaintenanceDisableConsole.php
+++ src/Spryker/Zed/Maintenance/Communication/Console/MaintenanceDisableConsole.php
@@ -36,13 +36,7 @@ class MaintenanceDisableConsole extends AbstractMaintenanceConsole
             ->addArgument(static::ARGUMENT_APPLICATION, InputArgument::OPTIONAL, 'Name of the application for which the maintenance mode should be disabled. (zed|yves)', static::APPLICATION_ALL);
     }
 
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return int
-     */
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
         $application = $this->getApplicationName($input);
 
